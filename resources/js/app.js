df4489ea9283050d02e634ba70f88f75b950231e
kazburrel <oegbosionu@gmail.com>
2024-09-22 15:10:13 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,37 @@
+import "./bootstrap";
+import "../css/app.css";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import GuestLayout from "./Layouts/GuestLayout.vue";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: async (name) => {
+        const page = await resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        );
+        if (name === "Welcome") {
+            page.default.layout = null;
+        } else {
+            page.default.layout =
+                page.default.layout ??
+                (typeof page.default.layout == null ? null : GuestLayout);
+        }
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: "#4B5563",
+    },
+});
