878bef83ded5c74f1825059b4fa9b9cec703f5e8
kazburrel <oegbosionu@gmail.com>
2024-10-26 19:04:52 +0100

    latest

--- test.js
+++ test.js
@@ -1,19 +1,7 @@
-// function findMissingNumber(nums) {
-//     const n = nums.length - 1;  // Adjust the length calculation
-//     const expectedSum = (n * (n + 1)) / 2;  // Calculate expected sum from 0 to n
-//     const actualSum = nums.map(num => acc + num, 0);  // Use map instead of reduce
-//     return expectedSum - actualSum;  // Return the difference
-// }
-
-// // Example usage:
-// const nums = [3, 0, 1];
-// const missingNumber = findMissingNumber(nums);
-// console.log(missingNumber); // Output: 2
-
 function findMissingNumber(nums) {
-    const n = nums.length; // Adjust the length calculation
+    const n = nums.length - 1; // Adjust the length calculation
     const expectedSum = (n * (n + 1)) / 2; // Calculate expected sum from 0 to n
-    const actualSum = nums.reduce((acc, num) => acc + num, 0); // Use reduce to calculate the sum
+    const actualSum = nums.map((num) => acc + num, 0); // Use map instead of reduce
     return expectedSum - actualSum; // Return the difference
 }
 
